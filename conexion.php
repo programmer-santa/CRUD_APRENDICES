9a520685d61a9765038a2867f929e2a764a89397
programador-santa <jairoporrasapple@gmail.com>
2025-04-11 19:20:11 -0500

    conexion a bases de datos

--- conexion.php
+++ conexion.php
@@ -1,18 +1,16 @@
 <?php
 
 $server = "localhost";
-$database = "prueba_db";
+$database = "aprendices";
 $usuario = "root";
 $contrasenia = "";
 
-$conexion = mysqli_connect($server, $usuario, $contrasenia, $database);
-
 try {
-    if (!$conexion) {
-        throw new Exception("Error de conexión: " . mysqli_connect_error());
-    } else {
-        // echo "Conexión exitosa a la base de datos.";
+    $conexion = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $usuario, $contrasenia);
+
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+    die("Error de conexion". $e->getMessage());
     }
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+
+?>
